fbb8be786501815edbe762db966ea25c751cd99e
XZH <ext.zhuo.xiong@tlscontact.com>
2021-09-20 11:06:04 +0800

    174, add transaction callback status check in tingg, XZH

--- app/PaymentGateway/TinggPaymentGateway.php
+++ app/PaymentGateway/TinggPaymentGateway.php
@@ -7,7 +7,9 @@ use App\Services\ApiService;
 use App\Services\GatewayService;
 use App\Services\PaymentService;
 use App\Services\TransactionService;
+use GuzzleHttp\Client;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class TinggPaymentGateway implements PaymentGatewayInterface
@@ -16,11 +18,13 @@ class TinggPaymentGateway implements PaymentGatewayInterface
     private $gatewayService;
     private $paymentService;
     private $apiService;
+    private $guzzleClient;
 
     public function __construct(
         TransactionService $transactionService,
         GatewayService $gatewayService,
         PaymentService $paymentService,
+        Client $guzzleClient,
         ApiService $apiService
     )
     {
@@ -28,6 +32,7 @@ class TinggPaymentGateway implements PaymentGatewayInterface
         $this->gatewayService     = $gatewayService;
         $this->paymentService     = $paymentService;
         $this->apiService         = $apiService;
+        $this->guzzleClient       = $guzzleClient;
     }
 
     public function getPaymentGatewayName()
@@ -62,8 +67,7 @@ class TinggPaymentGateway implements PaymentGatewayInterface
         $client       = $transaction['t_client'];
         $issuer       = $transaction['t_issuer'];
         $fg_id        = $transaction['t_xref_fg_id'];
-        $config       = $this->gatewayService->getGateway($client, $issuer, $this->getPaymentGatewayName());
-        $tingg_config = array_merge($config['common'], $this->isSandbox() ? $config['sandbox'] : $config['prod']);
+        $tingg_config = $this->getTinggConfig($transaction);
         $application  = $this->apiService->callTlsApi('GET', '/tls/v2/' . $client . '/form_group/' . $fg_id);
         $u_surname    = $application['body']['u_surname'] ?? '';
         $u_givenname  = $application['body']['u_givenname'] ?? '';
@@ -76,7 +80,7 @@ class TinggPaymentGateway implements PaymentGatewayInterface
             'serviceCode'           => $tingg_config['serviceCode'],
             'dueDate'               => Carbon::now()->addMinutes(30)->format('Y-m-d H:i:s'),
             'requestDescription'    => 'Tlscontact fees for group ' . $fg_id,
-            'countryCode'           => $this->isSandBox() ? 'KE' : strtoupper(substr($issuer, 0, 2)),
+            'countryCode'           => strtoupper(substr($issuer, 0, 2)),
             'languageCode'          => 'en',
             "customerLastName"      => $u_surname,
             "customerFirstName"     => $u_givenname,
@@ -91,7 +95,7 @@ class TinggPaymentGateway implements PaymentGatewayInterface
         $queryParams = [
             'accessKey'   => $tingg_config['accessKey'],
             'params'      => $encryptParams,
-            'countryCode' => $this->isSandBox() ? 'KE' : strtoupper(substr($issuer, 0, 2))
+            'countryCode' => strtoupper(substr($issuer, 0, 2))
         ];
         return [
             'form_method' => 'get',
@@ -102,14 +106,13 @@ class TinggPaymentGateway implements PaymentGatewayInterface
 
     public function return($params)
     {
-        if(empty($params['payments'])) {
+        if(empty($params['merchantTransactionID']) || empty($params['serviceCode'])) {
             return [
                 'status' => 'error',
                 'message' => 'no_data_received'
             ];
         }
 
-        $payments = current(json_decode($params['payments']));
         $transaction_id = str_replace('_', '-', $params['merchantTransactionID']);
         $this->paymentService->saveTransactionLog($transaction_id, $params, $this->getPaymentGatewayName());
 
@@ -122,15 +125,32 @@ class TinggPaymentGateway implements PaymentGatewayInterface
                 'message' => 'transaction_id_not_exists'
             ];
         }
+        $tingg_config = $this->getTinggConfig($transaction);
+        $bearer_token = $this->getAuthorization($tingg_config);
+        $response = $this->queryStatus($params, $bearer_token);
+        if(!empty($response['status']) && $response['status'] == 200) {
+            $payment = $response['body']['results'];
+            $confirm_params = [
+                'gateway' => $this->getPaymentGatewayName(),
+                'amount' => $payment['amountPaid'] ?? '',
+                'currency' => $payment['paymentCurrencyCode'] ?? 0,
+                'transaction_id' => $transaction_id,
+                'gateway_transaction_id' => current($payment['payments'])['payerTransactionID'] ?? '',
+            ];
+            return $this->paymentService->confirm($transaction, $confirm_params);
+        } else {
+            return [
+                'status' => 'error',
+                'message' => 'transaction_id_not_exists'
+            ];
+        }
+    }
 
-        $confirm_params = [
-            'gateway' => $this->getPaymentGatewayName(),
-            'amount' => $payments->amountPaid,
-            'currency' => $payments->currencyCode,
-            'transaction_id' => $transaction_id,
-            'gateway_transaction_id' => $payments->payerTransactionID,
-        ];
-        return $this->paymentService->confirm($transaction, $confirm_params);
+    private function getTinggConfig($transaction) {
+        $client       = $transaction['t_client'];
+        $issuer       = $transaction['t_issuer'];
+        $config       = $this->gatewayService->getGateway($client, $issuer, $this->getPaymentGatewayName());
+        return array_merge($config['common'], $this->isSandbox() ? $config['sandbox'] : $config['prod']);
     }
 
     private function encrypt($ivKey, $secretKey, $payload = [])
@@ -152,6 +172,49 @@ class TinggPaymentGateway implements PaymentGatewayInterface
         $encryptedPayload = base64_encode($encrypted);
 
         return $encryptedPayload;
+    }
+
+    private function getAuthorization($tingg_config) {
+        $data = [
+            'grant_type' => 'client_credentials',
+            'client_id' =>  $tingg_config['clientID'],
+            "client_secret" => $tingg_config['clientSecret'],
+        ];
+        $response = $this->guzzleClient->request('POST', 'https://developer.tingg.africa/checkout/v2/custom/oauth/token', [
+            'verify' => env('VERIFYPEER'),
+            'http_errors' => false,
+            'idn_conversion' => false,
+            'Accept' => 'application/json',
+            'json' => $data,
+        ]);
+        $response = [
+            'status' => $response->getStatusCode(),
+            'body' => json_decode($response->getBody(), true)
+        ];
+        return $response['body']['access_token'] ?? '';
+    }
+
+    private function queryStatus($params, $bearer_token) {
+        $data = [
+            'merchantTransactionID' => $params['merchantTransactionID'],
+            'serviceCode' => $params['serviceCode']
+        ];
+        $response = $this->guzzleClient->request('POST', 'https://developer.tingg.africa/checkout/v2/custom/requests/query-status', [
+            'verify' => env('VERIFYPEER'),
+            'http_errors' => false,
+            'idn_conversion' => false,
+            'Accept' => 'application/json',
+            'headers' => [
+                'Authorization' => 'Bearer ' . $bearer_token,
+                'Content-Type' => 'application/json'
+            ],
+            'json' => $data,
+        ]);
+        $response = [
+            'status' => $response->getStatusCode(),
+            'body' => json_decode($response->getBody(), true)
+        ];
+        return $response;
     }
 
 }

cf9c044b2075ca38268d4a191c396ace750a5c6b
XZH <ext.zhuo.xiong@tlscontact.com>
2021-09-27 14:48:28 +0800

    54, remove if from helper, XZH

--- app/Helpers/BasicFunctions.php
+++ app/Helpers/BasicFunctions.php
@@ -1,30 +1,26 @@
 <?php
 
-if (!function_exists('get_callback_url')) {
-    function get_callback_url(String $uri): string
-    {
-        return getenv('PAYMENT_SERVICE_DOMAIN') . $uri;
-    }
+function get_callback_url(string $uri): string
+{
+    return getenv('PAYMENT_SERVICE_DOMAIN') . $uri;
 }
 
-if (!function_exists('csvToArray')) {
-    function csvToArray($filename = '', $delimiter = "\t")
-    {
-        if (!file_exists($filename) || !is_readable($filename)) {
-            return [];
-        }
-        $header = null;
-        $data   = [];
-        if (($handle = fopen($filename, 'r')) !== false) {
-            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
-                if (!$header) {
-                    $header = $row;
-                } else {
-                    $data[] = array_combine($header, $row);
-                }
-            }
-            fclose($handle);
-        }
-        return $data;
+function csvToArray($filename = '', $delimiter = "\t")
+{
+    if (!file_exists($filename) || !is_readable($filename)) {
+        return [];
     }
+    $header = null;
+    $data   = [];
+    if (($handle = fopen($filename, 'r')) !== false) {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
+            if (!$header) {
+                $header = $row;
+            } else {
+                $data[] = array_combine($header, $row);
+            }
+        }
+        fclose($handle);
+    }
+    return $data;
 }

b37637dc87467eb7e22bf9695d26ddb573c3d4d0
shenxiuying <ext.xiuying.shen@tlscontact.com>
2022-04-14 10:21:49 +0800

    modify hmpo_uk payment

--- app/Services/GatewayService.php
+++ app/Services/GatewayService.php
@@ -15,27 +15,25 @@ class GatewayService
         return $all_gateway[$client][$issuer] ?? [];
     }
 
-    public function getGateway($client, $issuer, $gateway)
-    {
+    public function getGateway($client, $issuer, $gateway) {
         return config('payment_gateway')[$client][$issuer][$gateway] ?? [];
     }
 
-    public function getKbankConfig($client, $issuer, $gateway)
-    {
-        $kbank_config = $this->getGateway($client, $issuer, $gateway);
-        $app_env = env('APP_ENV') === 'production' ? false : true;
-        $is_live = $kbank_config['common']['env'] == 'live' ? true : false;
+    public function getKbankConfig($client, $issuer, $gateway) {
+        $kbank_config   = $this->getGateway($client, $issuer, $gateway);
+        $app_env        = env('APP_ENV') === 'production' ? false : true;
+        $is_live        = $kbank_config['common']['env'] == 'live' ? true : false;
         if ($is_live && !$app_env) {
             $config_data = [
                 'redirect_host' => $kbank_config['prod']['redirect_host'],
-                'api_key' => $kbank_config['prod']['apikey'],
-                'mid' => $kbank_config['prod']['mid']
+                'api_key'       => $kbank_config['prod']['apikey'],
+                'mid'           => $kbank_config['prod']['mid']
             ];
         } else {
             $config_data = [
                 'redirect_host' => $kbank_config['sandbox']['sandbox_redirect_host'],
-                'api_key' => $kbank_config['sandbox']['sandbox_apikey'],
-                'mid' => $kbank_config['sandbox']['sandbox_mid']
+                'api_key'       => $kbank_config['sandbox']['sandbox_apikey'],
+                'mid'           => $kbank_config['sandbox']['sandbox_mid']
             ];
         }
         return $config_data;

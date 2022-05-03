a8afb974cf11b2bef1b6117ab9858a3b8aa83bae
shenxiuying <ext.xiuying.shen@tlscontact.com>
2022-05-03 14:56:47 +0800

    add leg_be payment

--- config/payment_gateway.php
+++ config/payment_gateway.php
@@ -1526,6 +1526,78 @@ return [
             ]
         ]
 
+    ],
+    'leg_be' =>[
+        'zaAll2be' => [
+            'paypal' => [
+                'label' => 'PAYPAL pay',
+                'common' => [
+                    'env' => 'live',
+                    "activated" => true,
+                    "currency" => "EUR",
+                    "txn_fee_rate" => "0",
+                    "txn_fee_extra" => "0",
+                    "return_url" => "/paypal/return",
+                    "notify_url" => "/paypal/notify"
+                ],
+                'sandbox' => [
+                    "sandbox_host" => "https://www.sandbox.paypal.com/cgi-bin/webscr",
+                    "sandbox_account" => env("ENVPAY_PAY_COMMON_SANDBOX_ACCOUNT")
+                ],
+                'prod' => [
+                    "host" => "https://www.paypal.com/cgi-bin/webscr",
+                    "account" => env("ENVPAY_PAY_BEzaALL2be_ACCOUNT")
+                ]
+            ]
+        ],
+        'gbAll2be' => [
+            'globaliris' => [
+                'label' => 'GLOBALIRIS pay',
+                'common' => [
+                    'env' => 'live',
+                    "activated" => true,
+                    "currency" => "GBP",
+                    "min_fraction_digits" => "2",
+                    "txn_fee_rate" => "0",
+                    "txn_fee_extra" => "0",
+                    "return_url" => "/globaliris/return"
+                ],
+                'sandbox' => [
+                    "sandbox_host" => "https://hpp.sandbox.globaliris.com/pay",
+                    "sandbox_merchant_id" => env("ENVPAY_GLO_COMMON_SANDBOX_MERCHANT_ID"),
+                    "sandbox_secret" => env("ENVPAY_GLO_COMMON_SANDBOX_SECRET"),
+                    "sandbox_account" => ''
+                ],
+                'prod' => [
+                    "host" => "https://hpp.globaliris.com/pay",
+                    "merchant_id" => env("ENVPAY_GLO_COMMON_MERCHANT_ID"),
+                    "secret" => env("ENVPAY_GLO_COMMON_SECRET"),
+                    "account" => env("ENVPAY_GLO_BEgbALL2be_ACCOUNT")
+                ]
+            ]
+        ],
+        'ruMOW2be' => [
+            'paypal' => [
+                'label' => 'PAYPAL pay',
+                'common' => [
+                    'env' => 'live',
+                    "activated" => true,
+                    "currency" => "RUB",
+                    "txn_fee_rate" => "0",
+                    "txn_fee_extra" => "0",
+                    "return_url" => "/paypal/return",
+                    "notify_url" => "/paypal/notify"
+                ],
+                'sandbox' => [
+                    "sandbox_host" => "https://www.sandbox.paypal.com/cgi-bin/webscr",
+                    "sandbox_account" => env("ENVPAY_PAY_COMMON_SANDBOX_ACCOUNT")
+                ],
+                'prod' => [
+                    "host" => "https://www.paypal.com/cgi-bin/webscr",
+                    "account" => env("ENVPAY_PAY_BEruMOW2be_ACCOUNT")
+                ]
+            ]
+        ],
     ]
 
 ];

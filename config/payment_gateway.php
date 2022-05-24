7bd6c08f43c012d3147a63404ceeb8290936c2f0
shenxiuying <ext.xiuying.shen@tlscontact.com>
2022-05-24 14:51:47 +0800

    reset

--- config/payment_gateway.php
+++ config/payment_gateway.php
@@ -1530,29 +1530,6 @@ return [
                     "account" => env("ENVPAY_GLO_BEgbALL2be_ACCOUNT")
                 ]
             ],*/
-            'binga' => [
-                'label' => 'BINGA pay',
-                'common' => [
-                    'env' => 'live',
-                    "currency" => "MAD",
-                    "return_url" => "/binga/return",
-                    "notify_url" => "/binga/notify"
-                ],
-                'sandbox' => [
-                    "host" => "http://preprod.binga.ma:8080/v1.2/api/orders",
-                    "merchant_login" => env("ENVPAY_BINGA_SANDBOX_MERCHANT_LOGIN"),
-                    "merchant_password" => env("ENVPAY_BINGA_SANDBOX_MERCHANT_PASSWORD"),
-                    "store_id" => env("ENVPAY_BINGA_SANDBOX_STORE_ID"),
-                    "store_private_key" => env("ENVPAY_BINGA_SANDBOX_STORE_PRIVATE_KEY"),
-                ],
-                'prod' => [
-                    "host" => "https://api.binga.ma/bingaApi/api/orders",
-                    "merchant_login" => env("ENVPAY_BINGA_MERCHANT_LOGIN"),
-                    "merchant_password" => env("ENVPAY_BINGA_MERCHANT_PASSWORD"),
-                    "store_id" => env("ENVPAY_BINGA_STORE_ID"),
-                    "store_private_key" => env("ENVPAY_BINGA_STORE_PRIVATE_KEY"),
-                ]
-            ],
             'pay_later' => [
                 'label' => "Pay later",
             ]

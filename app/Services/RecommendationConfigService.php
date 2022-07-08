e485c1007987b8f04ab52dd8fff829bd838431c9
sxy <ext.xiuying.shen@tlscontact.com>
2022-07-08 16:29:00 +0800

    295 download rule engine

--- app/Services/RecommendationConfigService.php
+++ app/Services/RecommendationConfigService.php
@@ -20,4 +20,13 @@ class RecommendationConfigService
         return $this->recommendationConfigRepositories->insert($params);
     }
 
+    public function fetch() {
+        $select = ['rc_id', 'rc_file_name', 'rc_content', 'rc_file_size', 'rc_tech_creation'];
+        return $this->recommendationConfigRepositories->fetch($select);
+    }
+
+    public function fetchByRcId($rc_id) {
+        return $this->recommendationConfigRepositories->fetchByRcId($rc_id);
+    }
+
 }

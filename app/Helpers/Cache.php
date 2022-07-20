196688984adec10d978a1b4a392590359c3967cc
WYH <ext.yahui.wang@tlscontact.com>
2022-07-20 11:48:52 +0800

    upddate cache

---
+++ app/Helpers/Cache.php
@@ -0,0 +1,28 @@
+<?php
+
+function get_application_action_history_cache_key($f_id): string
+{
+    return 'application_action_history_cache_' . app()->make(App\Services\ApiService::class)->getProjectId() . '_' . $f_id;
+}
+
+function get_form_stage_status_cache_key($f_id): string
+{
+    return 'form_stage_status_cache_' . app()->make(App\Services\ApiService::class)->getProjectId() . '_' . $f_id;
+}
+
+function get_cache_ttl($type)
+{
+    switch ($type) {
+        case 'applicant':
+            $ttl = 5 * 60;
+            break;
+        case 'visa_type':
+        case 'configuration':
+        case 'issuer':
+            $ttl = 30 * 60;
+            break;
+        default:
+            $ttl = 15 * 60;
+    }
+    return $ttl;
+}

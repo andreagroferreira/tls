28e2c06a7e9383a5603d691fbc883e4bb59bf472
yangyang <ext.yangyang.xue@tlscontact.com>
2022-07-22 15:16:04 +0800

    323 security updates yangyang

--- app/PaymentGateway/AlipayPaymentGateway.php
+++ app/PaymentGateway/AlipayPaymentGateway.php
@@ -98,7 +98,7 @@ class AlipayPaymentGateway implements PaymentGatewayInterface
         ksort($post_params);
         //dd($post_params);
         $tmp = urldecode(http_build_query($post_params));
-        $private_res = config('payment_service')['begin_rsa_private_key'] . PHP_EOL . wordwrap($private_key, 64, "\n", true) . PHP_EOL . config('payment_service')['end_rsa_private_key'];
+        $private_res = env('BEGIN_RSA_PRIVATE_KEY') . PHP_EOL . wordwrap($private_key, 64, "\n", true) . PHP_EOL . env('END_RSA_PRIVATE_KEY');
         // 生成的签名
         $private_sign = openssl_sign($tmp, $sign, $private_res, OPENSSL_ALGO_SHA256) ? base64_encode($sign) : null;
         //$post_params['sign'] = $private_sign;
--- config/payment_service.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'begin_rsa_private_key' =>  "-----BEGIN RSA PRIVATE KEY-----",
-    'end_rsa_private_key'   =>  "-----END RSA PRIVATE KEY-----",
-    'begin_public_key'      =>  "-----BEGIN PUBLIC KEY-----",
-    'end_public_key'        =>  "-----END PUBLIC KEY-----",
-];

99f960e4423c4d8ac786ed09d29a333e5cc09c03
yangyang <ext.yangyang.xue@tlscontact.com>
2022-08-03 23:51:06 +0800

    351 Configure payment gateway details yangyang

---
+++ app/Repositories/PaymentAccountsRepositories.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Models\PaymentAccounts;
+use Illuminate\Support\Facades\DB;
+
+class PaymentAccountsRepositories
+{
+    protected $paymentAccounts;
+
+    public function __construct(PaymentAccounts $paymentAccounts)
+    {
+        $this->paymentAccounts = $paymentAccounts;
+    }
+
+    public function setConnection($connection)
+    {
+        $this->paymentAccounts->setConnection($connection);
+    }
+
+    public function getConnection()
+    {
+        return $this->paymentAccounts->getConnectionName();
+    }
+
+    public function fetch($where, $field = '*')
+    {
+        return $this->paymentAccounts
+            ->select($field)
+            ->where($where)
+            ->get();
+    }
+
+    public function create($attributes)
+    {
+        return $this->paymentAccounts->create($attributes);
+    }
+
+    public function update($where, $attributes)
+    {
+
+        $paymentAccounts = $this->paymentAccounts->where($where)->first();
+        if (blank($paymentAccounts)) {
+            return false;
+        }
+
+        foreach ($attributes as $key => $value) {
+            $paymentAccounts->$key = $value;
+        }
+        $paymentAccounts->save();
+
+        return $this->paymentAccounts->find($paymentAccounts->pa_id);
+    }
+
+    public function findBy($attributes) {
+        $result = $this->paymentAccounts;
+        foreach ($attributes as $key => $value) {
+            $result = $result->where($key, '=', $value);
+        }
+        return $result->get();
+    }
+}

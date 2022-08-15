ff5766ad8d388e4eb59756d9a85f39ec7c0c2034
LZ <ext.zhen.liu@tlscontact.com>
2022-08-16 01:12:16 +0800

    392_fix_review_issue LZ

--- app/Services/PaymentConfigurationsService.php
+++ app/Services/PaymentConfigurationsService.php
@@ -59,19 +59,22 @@ class PaymentConfigurationsService
         ]);
         foreach ($payment_configurations->toArray() as $payment_config) {
             $country = $payment_config['pc_country'];
-            $city = $payment_config['pc_city'];
+            $city    = $payment_config['pc_city'];
             $res_key = $country . '-' . $city;
             $account = $this->paymentAccountsRepositories->fetch(['pa_id' => $payment_config['pc_xref_pa_id']]);
-            if ($account) {
+            $payment = [
+                'pc_id'   => $payment_config['pc_id'],
+                'country' => $country,
+                'city'    => $city,
+                'service' => []
+            ];
+            if (empty($account)) {
+                $result[$res_key] = $payment;
+            } else {
                 if (isset($result[$res_key])) {
-                    $result[$res_key]['service'] = $result[$res_key]['service'] . ', ' . $account->pa_name;
+                    $result[$res_key]['service'][$account->pa_id] = $account->pa_name;;
                 } else {
-                    $payment = [
-                        'pc_id' => $payment_config['pc_id'],
-                        'country' => $country,
-                        'city' => $city,
-                        'service' => $account->pa_name
-                    ];
+                    $payment['service'][$account->pa_id] = $account->pa_name;
                     $result[$res_key] = $payment;
                 }
             }

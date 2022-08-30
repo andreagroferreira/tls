ea5efa7eee6c2125a5e90254b0e3c344bef076b5
LZ <ext.zhen.liu@tlscontact.com>
2022-08-30 10:21:36 +0800

    392_fix_review_issues LZ

--- database/migrations/2022_08_29_105026_add_payment_method_for_payment_service_providers_table.php
+++ database/migrations/2022_08_29_105026_add_payment_method_for_payment_service_providers_table.php
@@ -14,22 +14,24 @@ class AddPaymentMethodForPaymentServiceProvidersTable extends Migration
      */
     public function up()
     {
-        $condition = [
-            'psp_code' => 'cybersource',
-            'psp_name' => 'cybersource pay'
-        ];
-        DB::beginTransaction();
-        try {
-            $payment_service = DB::table('payment_service_providers')->where($condition)->first();
-            if (empty($payment_service)) {
-                DB::table('payment_service_providers')->insert($condition);
-            } else {
-                DB::table('payment_service_providers')->where('psp_id', '=', $payment_service->psp_id)->update($condition);
+        if (DB::getDefaultConnection() == 'deploy_payment_pgsql') {
+            $condition = [
+                'psp_code' => 'cybersource',
+                'psp_name' => 'cybersource pay'
+            ];
+            DB::beginTransaction();
+            try {
+                $payment_service = DB::table('payment_service_providers')->where($condition)->first();
+                if (empty($payment_service)) {
+                    DB::table('payment_service_providers')->insert($condition);
+                } else {
+                    DB::table('payment_service_providers')->where('psp_id', '=', $payment_service->psp_id)->update($condition);
+                }
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                throw new LogicException($e->getMessage());
             }
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw new LogicException($e->getMessage());
         }
     }
 

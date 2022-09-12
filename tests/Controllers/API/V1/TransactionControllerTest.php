5dbbb41d44e13c4d2a6fd3751bd39d4390bb37b2
Daniel Sartor <danielsartor@daniels-mbp.lan>
2022-09-12 17:23:53 +0100

    WIP Transaction Tests

--- tests/Controllers/API/V1/TransactionControllerTest.php
+++ tests/Controllers/API/V1/TransactionControllerTest.php
@@ -2,189 +2,254 @@
 
 namespace Tests\Controllers\API\V1;
 
+use App\Models\Transactions;
 use Illuminate\Support\Carbon;
 
 class TransactionControllerTest extends TestCase
 {
-    public function testFetch()
+    /**
+     * @var string
+     */
+    private string $baseApiUrl = 'api/v1/transaction';
+
+    /**
+     * @return void
+     */
+    public function testTransactionApiMethodIsPost(): void
     {
-        $base_url = 'api/v1/transaction';
-
-        $this->get($base_url);
+        $this->get($this->baseApiUrl);
         $this->response->assertStatus(405);
+    }
 
-        $this->get($base_url . '/test');
+    /**
+     * @return void
+     */
+    public function testGetTransactionApiParameterType(): void
+    {
+        $this->get($this->baseApiUrl . '/test');
         $this->response->assertStatus(400)
             ->assertJson([
                 'error' => 'params error',
                 'message' => 'The fg id must be an integer.',
             ]);
-
-        $this->get($base_url . '/1');
-        $this->response->assertStatus(204);
-
-        $transaction = $this->generateTransaction();
-        $this->generateTransactionItems($transaction->t_transaction_id);
-        $transaction_items = $this->generateTransactionItems($transaction->t_transaction_id);
-
-        $this->get($base_url . '/' . $transaction->t_xref_fg_id);
-        $response_array = $this->response->decodeResponseJson();
-        $this->response->assertStatus(200);
-        $this->assertEquals(1, count($response_array));
-        $this->assertEquals(1, array_get($response_array, '0.t_id'));
-        $this->assertEquals($transaction->t_transaction_id, array_get($response_array, '0.transaction_id'));
-        $this->assertEquals('pending', array_get($response_array, '0.status'));
-        $this->assertEquals(1, count(array_get($response_array, '0.items')));
-        $this->assertEquals($transaction_items->ti_xref_f_id, array_get($response_array, '0.items.0.f_id'));
-        $this->assertEquals(2, count(array_get($response_array, '0.items.0.skus')));
     }
 
-    public function testCreate()
+    /**
+     * @return void
+     */
+    public function testGetTransactionThatDoesNotExists(): void
     {
-        $base_url = 'api/v1/transaction';
+        $this->get($this->baseApiUrl . '/1');
+        $this->response->assertStatus(204);
+    }
 
-        $this->post($base_url);
+    /**
+     * @return void
+     * @throws \Throwable
+     */
+    public function testGetSuccessfulTransactionFromDatabase(): void
+    {
+        $transaction = $this->generateTransaction();
+        $this->generateTransactionItems($transaction->t_transaction_id);
+
+        $this->get($this->baseApiUrl . '/' . $transaction->t_xref_fg_id);
+        $this->response->assertStatus(200);
+
+        $response_array = $this->response->decodeResponseJson();
+
+        $this->assertNotEmpty($response_array);
+        $this->assertNotEmpty(array_get($response_array, '0.items'));
+    }
+
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     */
+    public function testCreateTransactionPrimaryFields(array $defaultPayload): void
+    {
+        $payload = [];
+        foreach ($defaultPayload as $field => $value) {
+            $this->post($this->baseApiUrl, $payload);
+            $this->response->assertStatus(400)
+                ->assertJson([
+                    'error' => 'params error',
+                    'message' => 'The ' . str_replace('_', ' ', $field) . ' field is required.',
+                ]);
+
+            $payload[$field] = $value;
+        }
+    }
+
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     */
+    public function testCreateTransactionWithEmptyItems(array $defaultPayload): void
+    {
+        $defaultPayload['items'] = [];
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200);
+    }
+
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     */
+    public function testCreateTransactionItemsFields(array $defaultPayload): void
+    {
+        //Validate missing items.skus
+        $defaultPayload['items'] = [[]];
+        $this->post($this->baseApiUrl, $defaultPayload);
         $this->response->assertStatus(400)
             ->assertJson([
                 'error' => 'params error',
-                'message' => 'The fg id field is required.'
+                'message' => 'The items.skus field is required.',
             ]);
 
-        $post_data = ['fg_id' => 'test'];
-        $this->post($base_url, $post_data);
+        //Validate missing items.skus.sku, price and vat
+        $defaultPayload['items'] = [['skus' => [[]]]];
+        $this->post($this->baseApiUrl, $defaultPayload);
         $this->response->assertStatus(400)
             ->assertJson([
                 'error' => 'params error',
-                'message' => 'The fg id must be an integer.'
+                'message' => 'The items.skus.sku, price, vat field is required.',
             ]);
 
-        $post_data = ['fg_id' => 10000];
-        $this->post($base_url, $post_data);
+        //Validate missing items.fg_id
+        $defaultPayload['items'] = [['skus' => [['sku' => 1, 'price' => 1, 'vat' => 1]]]];
+        $this->post($this->baseApiUrl, $defaultPayload);
         $this->response->assertStatus(400)
             ->assertJson([
                 'error' => 'params error',
-                'message' => 'The client field is required.'
+                'message' => 'The items.f_id must be an integer.',
             ]);
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be'];
-        $this->post($base_url, $post_data);
+        //Validate string items.fg_id
+        $defaultPayload['items'] = [['skus' => [['sku' => 1, 'price' => 1, 'vat' => 1]], 'fg_id' => 'test']];
+        $this->post($this->baseApiUrl, $defaultPayload);
         $this->response->assertStatus(400)
             ->assertJson([
                 'error' => 'params error',
-                'message' => 'The issuer field is required.'
+                'message' => 'The items.f_id must be an integer.',
             ]);
+    }
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The currency field is required.'
-            ]);
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    public function testCreateTransactionIsSuccessful(array $defaultPayload): void
+    {
+        //Create Transaction
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200)
+            ->assertJsonStructure(['t_id', 'expire']);
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The redirect url field is required.'
-            ]);
+        $postResponse = $this->response->decodeResponseJson();
+        $this->assertTrue(Carbon::parse($this->getDbNowTime())->lt(array_get($postResponse, 'expire')));
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The onerror url field is required.'
-            ]);
+        //Get Created Transaction
+        $this->get($this->baseApiUrl . '/' . $defaultPayload['fg_id']);
+        $this->response->assertStatus(200);
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The reminder url field is required.'
-            ]);
+        $transactionData = $this->response->decodeResponseJson();
+        $this->assertNotEmpty($transactionData);
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The reminder url field is required.'
-            ]);
+        $this->assertEquals($postResponse['t_id'], array_get($transactionData, '0.t_id'));
+        $this->assertEquals('pending', array_get($transactionData, '0.status'));
+        $this->assertNotEmpty(array_get($transactionData, '0.items'));
+    }
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The callback url field is required.'
-            ]);
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    public function testCreateMultipleTransactionsWithSameData(array $defaultPayload): void
+    {
+        //Create First Transaction
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200)
+            ->assertJsonStructure(['t_id', 'expire']);
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The workflow field is required.'
-            ]);
+        $firstTransactionPost = $this->response->decodeResponseJson();
 
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac'];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The items field is required.'
-            ]);
+        //Create Second Transaction
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200)
+            ->assertJsonStructure(['t_id', 'expire']);
+        $secondTransactionPost = $this->response->decodeResponseJson();
 
-        $items = json_encode([
-            ['test' => 'test']
-        ]);
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac', 'items' => $items];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The items.skus field is required.'
-            ]);
+        //Get Created Transaction
+        $this->get($this->baseApiUrl . '/' . $defaultPayload['fg_id']);
+        $this->response->assertStatus(200);
 
-        $items = json_encode([
-            ['test' => 'test', 'skus' => [['sku' => 1]]]
-        ]);
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac', 'items' => $items];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The items.skus.price, vat field is required.'
-            ]);
+        $transactionData = $this->response->decodeResponseJson();
 
-        $items = json_encode([
-            ['test' => 'test', 'skus' => [['sku' => 1, 'price' => 1]]]
-        ]);
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac', 'items' => $items];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The items.skus.vat field is required.'
-            ]);
+        $this->assertCount(1, $transactionData);
+        $this->assertEquals(array_get($firstTransactionPost, 't_id'), array_get($secondTransactionPost, 't_id'));
+        $this->assertEquals(1, array_get($transactionData, '0.t_id'));
+    }
 
-        $items = json_encode([
-            ['f_id' => '', 'skus' => [['sku' => 1, 'price' => 1, 'vat' => 1]]]
-        ]);
-        $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac', 'items' => $items];
-        $this->post($base_url, $post_data);
-        $this->response->assertStatus(400)
-            ->assertJson([
-                'error' => 'params error',
-                'message' => 'The items.f_id must be an integer.'
-            ]);
+    /**
+     * @dataProvider defaultPayload
+     *
+     * @param array $defaultPayload
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    public function testCreateMultipleTransactionsWithSameFormGroupAndDifferentItems(array $defaultPayload): void
+    {
+        //Create First Transaction
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200)
+            ->assertJsonStructure(['t_id', 'expire']);
 
-        $items = json_encode([
-            ['f_id' => 10001, 'skus' => [['sku' => 1, 'price' => 1, 'vat' => 1]]]
-        ]);
+        $firstTransactionPost = $this->response->decodeResponseJson();
+
+        //Create Second Transaction
+        $defaultPayload['items'] = [['f_id' => 10001, 'skus' => [['sku' => 2, 'price' => 2, 'vat' => 2]]]];
+        $this->post($this->baseApiUrl, $defaultPayload);
+        $this->response->assertStatus(200)
+            ->assertJsonStructure(['t_id', 'expire']);
+        $secondTransactionPost = $this->response->decodeResponseJson();
+
+        //Get Created Transaction
+        $this->get($this->baseApiUrl . '/' . $defaultPayload['fg_id']);
+        $this->response->assertStatus(200);
+
+        $transactionData = $this->response->decodeResponseJson();
+
+        $this->assertCount(1, $transactionData);
+        $this->assertNotEquals(array_get($firstTransactionPost, 't_id'), array_get($secondTransactionPost, 't_id'));
+
+        //Assert id of the transaction received is the second created
+        $this->assertNotEquals(array_get($firstTransactionPost, 't_id'), array_get($transactionData, '0.t_id'));
+        $this->assertEquals(array_get($secondTransactionPost, 't_id'), array_get($transactionData, '0.t_id'));
+    }
+
+    public function Create()
+    {
         $post_data = ['fg_id' => 10000, 'client' => 'be', 'issuer' => 'dzALG2fr', 'currency' => 'MAD', 'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr', 'onerror_url' => 'onError_tlsweb_url?lang=fr-fr', 'reminder_url' => 'callback_to_send_reminder?lang=fr-fr', 'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr', 'workflow' => 'vac', 'items' => $items];
         $this->post($base_url, $post_data);
         $this->response->assertStatus(200);
@@ -237,7 +302,7 @@ class TransactionControllerTest extends TestCase
         $this->response->assertStatus(204);
     }
 
-    public function testFetchAll()
+    public function FetchAll()
     {
         $base_url = 'api/v1/transactions';
 
@@ -402,5 +467,36 @@ class TransactionControllerTest extends TestCase
         $response_array = $this->response->decodeResponseJson();
         $this->assertEquals(0, array_get($response_array, 'total'));
         $this->assertEquals([], array_get($response_array, 'data'));
+    }
+
+    public function defaultPayload(): array
+    {
+        return [
+            [
+                [
+                    'fg_id' => 10000,
+                    'client' => 'be',
+                    'issuer' => 'dzALG2fr',
+                    'currency' => 'EUR',
+                    'redirect_url' => 'onSuccess_tlsweb_url?lang=fr-fr',
+                    'onerror_url' => 'onError_tlsweb_url?lang=fr-fr',
+                    'reminder_url' => 'callback_to_send_reminder?lang=fr-fr',
+                    'callback_url' => 'receipt_url/{fg_id}?lang=fr-fr',
+                    'workflow' => 'vac',
+                    'items' => [
+                        [
+                            'skus' => [
+                                [
+                                    'sku' => 1,
+                                    'price' => 1,
+                                    'vat' => 1,
+                                ],
+                            ],
+                            'f_id' => 10001
+                        ],
+                    ],
+                ]
+            ],
+        ];
     }
 }

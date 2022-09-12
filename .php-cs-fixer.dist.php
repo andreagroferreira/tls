92e4f2d9b392221659c538475259c449af336aeb
Daniel Sartor <danielsartor@daniels-mbp.lan>
2022-09-12 21:33:09 +0100

    PhpCsFixer rules

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('somedir')
+    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+    ->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    '@PhpCsFixer' => true,
+    'strict_param' => true,
+    'no_superfluous_phpdoc_tags' => false,
+    'phpdoc_no_empty_return' => false,
+    'phpdoc_line_span' => ['property' => 'single'],
+    'array_syntax' => ['syntax' => 'short'],
+])->setFinder($finder);

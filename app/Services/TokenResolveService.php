275cfd22fc4db2327c626d38a4e44cc1645430a2
Akshata Potdar <akshata.potdar.ext@tlscontact.com>
2022-09-13 11:23:55 +0530

    Changes made as per suggestions given and also added application token translation

--- app/Services/TokenResolveService.php
+++ app/Services/TokenResolveService.php
@@ -5,209 +5,329 @@ namespace App\Services;
 
 class TokenResolveService
 {
-    protected $directusService;
+    /**
+     * @var DirectusService
+     * @var ApiService
+     */
+    protected DirectusService $directusService;
+    protected ApiService $apiService;
+    /**
+     * @var string
+     */
+    private string $issuer;
 
-    public function __construct(DirectusService $directusService){
+    /**
+     * @var string
+     */
+    private string $country;
+
+    /**
+     * @var string
+     */
+    private string $city;
+
+    /**
+     * @var string
+     */
+    private string $client;
+
+    /**
+     * @param DirectusService $directusService
+     * @param ApiService $apiService
+     */
+    public function __construct(DirectusService $directusService, ApiService $apiService)
+    {
         $this->directusService = $directusService;
+        $this->apiService = $apiService;
     }
 
-    // to get template which needs to be resolved
-    public function getTemplateData($item,$issuer){
-        $country = substr($issuer, 0,2);
-        $city = substr($issuer, 2,3);
+    /**
+     * Get Directus template for test purspose
+     *
+     * @param string $collection
+     * @param string $lang
+     * @param string $issuer
+     * @param string $fg_id
+     * @return array
+     * @throws \Exception
+     */
+    public function getTemplateData(string $collection, string $issuer, string $lang, string $fg_id): array
+    {
+        $this->issuer = $issuer;
+        $this->country = substr($this->issuer, 0,2);
+        $this->city = substr($this->issuer, 2,3);
         $filters = [
             'code'=>[
-                'in' => [$city,$country,'ww']
+                'in' => [
+                    $this->city,
+                    $this->country,
+                    'ww'
+                ]
             ],
             'status' => [
                 'eq' => 'published'
             ]
         ];
         $select = 'code, translation.email_title, translation.email_content, translation.invoice_content, translation.activation';
-        $options['lang'] = 'en-us';
-        $getTemplate = $this->directusService->getContent($item, $select, $filters, $options);
-        $data = $this->resolveTemplate($getTemplate, $issuer);
-        return $data;
-        
+        $options['lang'] = $lang;
+        $getTemplate = $this->directusService->getContent(
+            $collection,
+            $select,
+            $filters,
+            $options
+        );
+        return $this->resolveTemplate($getTemplate,$issuer, $lang, $fg_id);
     }
 
-    // will resolved the template based on collection tokens
-    public function resolveTemplate($template, $issuer){
+    /**
+     * @param array $template
+     * @param string $issuer
+     * @param string $lang
+     * @param string $fg_id
+     * @return array
+     * @throws \Exception
+     */
+
+    public function resolveTemplate(array $template,  string $issuer, string $lang, string $fg_id): array
+    {
         $data = [];
-        $resolved_tokens = [];
-        $final_tokens = [];
-        $emailcontent = '';
-        $invoicecontent = '';
-        if(is_array($template) and count($template)){
-            $data = $this->getEmailInvoiceContent($template,$issuer);
-
-            if(isset($data['email_content'])){
-                $emailcontent = $data['email_content'];
-            }
-            if(isset($data['invoice_content'])){
-                $invoicecontent = $data['invoice_content'];
-            }
-            $pattern = "~({{\w+:\w+:\w+}}|{{\w+:\w+}})~";
-            if($emailcontent != ''){
-                $final_tokens = $this->pregMatchTemplate($data,$pattern);
-            }
-            if(count($final_tokens)){
-                $resolved_tokens = $this->getResolvedTokens($final_tokens,$issuer);
-                if(count($resolved_tokens)){
-                    ksort($resolved_tokens);
-                    foreach ($resolved_tokens as $key => $value) {
-                       $data['email_content'] = str_replace($final_tokens[$key], $value, $emailcontent);
-                       $data['invoice_content'] = str_replace($final_tokens[$key], $value, $invoicecontent);
-                       $emailcontent = $data['email_content'];
-                       $invoicecontent = $data['invoice_content'];
-                    }
-                }
-            }
-
+        $this->issuer = $issuer;
+        $this->country = substr($this->issuer, 0, 2);
+        $this->city = substr($this->issuer, 2, 3);
+        $this->client = substr($this->issuer, 6, 2);
+        if (empty($template)) {
+            return $data;
         }
+        $data = $this->getCorrectCollectionTranslation($template);
+
+        if(empty($data['email_content']) && empty($data['invoice_content'])) {
+            return $data;
+        }
+
+        $listOfToken = $this->pregMatchTemplate($data);
+
+        if (empty($listOfToken)) {
+            return $data;
+        }
+
+        $resolvedTokens = $this->getResolvedTokens($listOfToken, $lang, $fg_id);
+
+        if (empty($resolvedTokens)) {
+            throw new \Exception('Token were not resolved');
+        }
+
+        foreach ($resolvedTokens as $token => $value) {
+            $data['email_content'] = str_replace($token, $value, $data['email_content']);
+            $data['invoice_content'] = str_replace($token, $value, $data['invoice_content']);
+        }
+
         return $data;
     }
 
-    //returns the list of tokens after preg_match with given pattern
-    private function pregMatchTemplate($content, $pattern){
+    /**
+     * returns the list of tokens after preg_match with given pattern
+     *
+     * @param array $content
+     *
+     * @return array
+     */
+    private function pregMatchTemplate(array $content): array
+    {
+        $pattern = "~({{\w+:\w+:\w+}}|{{\w+:\w+}})~";
+
         preg_match_all($pattern, $content['email_content'], $email_tokens);
         preg_match_all($pattern, $content['invoice_content'], $invoice_tokens);
-        if(count($email_tokens)){
+        if(count($email_tokens)) {
             $tokens[] = array_unique($email_tokens, SORT_REGULAR)[0];
-        }        
-        if(count($invoice_tokens)){
+        }
+        if(count($invoice_tokens)) {
             $tokens[] = array_unique($invoice_tokens, SORT_REGULAR)[0];
 
-        }    
-        // will hold all tokens from email and invoice content 
-        $final_tokens  = array_unique($tokens, SORT_REGULAR)[0];
-        return $final_tokens;
+        }
+        // will hold all tokens from email and invoice content
+        return array_unique($tokens, SORT_REGULAR)[0];
     }
 
-    //returns the list of resolved tokens 
-    private function getResolvedTokens($tokens,$issuer){
-        $resolvedtokens = [];
+    /**
+     * returns the list of resolved tokens
+     *
+     * @param array $tokens
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    private function getResolvedTokens(array $tokens, string $lang, string $fg_id): array
+    {
         $resolved_tokens = [];
-        $country = substr($issuer, 0, 2);
-        $city = substr($issuer, 2, 3);
-        foreach ($tokens as $tkey => $tok) {
-            $val = str_replace(array('{{','}}'), '', $tok);
-            $arr = explode(':', $val);
 
-            if($arr[0] == 'c'){ // if collection token - directus
-                if($arr[2] == 'address'){
-                    $issuer_filter = [$issuer,'ww'];
-                }
-                else {
-                     $issuer_filter = [$city,$country,'ww'];
-                }
-                $filters = [
-                    'code'=>[
-                        'in' =>  $issuer_filter
-                    ],
-                    'status' => [
-                        'eq' => 'published'
-                    ],
-                ];
-                $resolvedtokens[$tkey] = $this->getTokenTranslationFromDirectus($arr,$filters,$country,$city);
+        foreach ($tokens as $token) {
+            /*
+             * Token structure
+             * {{collection : collection_name : field_name}}
+             * {{application : field_name}}
+             */
+            $token_details = explode(':', str_replace(array('{{','}}'), '', $token));
+
+            if($token_details[0] == 'c') {  /* if collection token - directus */
+                $resolved_tokens[$token] = $this->getTokenTranslationFromDirectus(
+                    $token_details,
+                    $lang
+                );
+            }
+            else if($token_details[0] == 'a') { /* if application token - api call */
+                $resolved_tokens[$token] = $this->getTokenTranslationFromApplication($token_details,$fg_id);
             }
         }
-        if(count($resolvedtokens)){
-            foreach ($resolvedtokens as $r => $res) {
-                if(isset($res[0]) and $res[0] != ''){
-                     $resolved_tokens[$r] = $res[0];
-                } elseif(isset($res[1]) and $res[1] != ''){
-                     $resolved_tokens[$r] = $res[1];
-                } elseif(isset($res[2]) and $res[2] != ''){
-                     $resolved_tokens[$r] = $res[2];
-                } elseif(isset($res[99]) and $res[99] != ''){
-                    $resolved_tokens[$r] = $res[99];
-                }
 
-            }
-        }        
         return $resolved_tokens;
     }
 
-    // get best matched collection item based on city,country & ww
-    private function getEmailInvoiceContent($template,$issuer){
-        $contentdata = [];
-        $country = substr($issuer, 0, 2);
-        $city = substr($issuer, 2, 3);
-        $continue = 0;
-        foreach ($template as $key => $value) {
-            if(isset($value['translation']) and count($value['translation'])){
-                $continue = 1;
-                foreach ($value['translation'] as $k => $val) {
-                    if($val['activation'] == true){
-                        if($value['code'] == $city){
-                            $email_title[0] = $val['email_title'];
-                            $email_content[0] = $val['email_content'];
-                            $invoice_content[0] = $val['invoice_content'];
-                        }
-                        else if($value['code'] == $country){
-                            $email_title[1] = $val['email_title'];
-                            $email_content[1] = $val['email_content'];
-                            $invoice_content[1] = $val['invoice_content'];
-                        }
-                        else if($value['code'] == 'ww'){
-                            $email_title[2] = $val['email_title'];
-                            $email_content[2] = $val['email_content'];
-                            $invoice_content[2] = $val['invoice_content'];
-                        }
-                    }
-                    
-                }
+    /**
+     *  get best matched collection item based on city,country & ww
+     *
+     * @param array $collections
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    private function getCorrectCollectionTranslation(
+        array $collections
+    ): array
+    {
+        $numberOfCollections = count($collections);
+        $globalOnly = $numberOfCollections === 1;
+        $hasCity = $numberOfCollections > 2;
+
+        $collectionIndex = null;
+        foreach ($collections as $i => $collection) {
+            $code = $collection['code'];
+            if($code == $this->city) {
+                $collectionIndex = $i;
+                break;
+            }
+            else if(!$hasCity && $code == $this->country) {
+                $collectionIndex = $i;
+                break;
+            }
+            else if($globalOnly && $code == 'ww') {
+                $collectionIndex = $i;
             }
         }
-        if($continue == 1){
-            if(isset($email_content[0]) and $email_content[0] != ''){
-                $emailtitle = $email_title[0];
-                $emailcontent = $email_content[0];
-                $invoicecontent = $invoice_content[0];
-            } elseif (isset($email_content[1]) and $email_content[1] != '') {
-                $emailtitle = $email_title[1];
-                $emailcontent = $email_content[1];
-                $invoicecontent = $invoice_content[1];
-            } elseif (isset($email_content[2]) and $email_content[2] != '') {
-                $emailtitle = $email_title[2];
-                $emailcontent = $email_content[2];
-                $invoicecontent = $invoice_content[2];
-            }
-            $contentdata['email_title'] =  $emailtitle;
-            $contentdata['email_content'] =  $emailcontent;
-            $contentdata['invoice_content'] =  $invoicecontent;
+
+        if ($collectionIndex === null) {
+            throw new \Exception('Correct collection index not found');
         }
-        return $contentdata;
+
+        $translation = $this->getActiveTranslation($collections[$collectionIndex]['translation']);
+
+        if (empty($translation)) {
+            throw new \Exception('No active translation found');
+        }
+
+        return $translation;
     }
 
-    //calls directus collection to get translation of Token
-    private function getTokenTranslationFromDirectus($arr,$filters,$country,$city){
-        $collection = $arr[1];
-        $field = 'translation.'.$arr[2];
-        $options['lang'] = 'en-us';
-        $select = "code,".$field;
-        $resolvedtokens = [];
-        $getTokenval = $this->directusService->getContent($collection, $select, $filters, $options);
-        if(count($getTokenval)){
-            foreach ($getTokenval as $key => $value) {
-                foreach ($value['translation'] as $k => $val) {
-                    if($arr[2] == 'address'){
-                        $resolvedtokens[99] = $val[$arr[2]];
-                    }
-                    else if($value['code'] == $city) {
-                        $resolvedtokens[0] = $val[$arr[2]];
-                    }
-                    else if($value['code'] == $country) {
-                        $resolvedtokens[1] = $val[$arr[2]];
-                    }
-                    else if($value['code'] == 'ww') {
-                        $resolvedtokens[2] = $val[$arr[2]];
-                    }
-                }
-            } 
+    /**
+     * @param array $translations
+     *
+     * @return array
+     */
+    private function getActiveTranslation(array $translations): array
+    {
+        foreach ($translations as $translation) {
+            if (!isset($translation['activation'])) {
+                return $translation;
+            }
+            if ($translation['activation']) {
+                return $translation;
+            }
         }
-        return $resolvedtokens;
+
+        return [];
+    }
+
+    /**
+     * calls directus collection to get translation of Token
+     *
+     * @param array $token_details
+     * @param array $issuer_filter
+     *
+     * @return string
+     *
+     * @throws \Exception
+     */
+    private function getTokenTranslationFromDirectus(
+        array $token_details,
+        string $lang
+    ): string
+    {
+        $collection = $token_details[1];
+        $field = 'translation.'.$token_details[2];
+        $options['lang'] = $lang;
+        $select = "code," . $field;
+        $issuer_filter = [
+            $this->city,
+            $this->country ,
+            'ww'
+        ];
+
+        if($token_details[1] == 'application_centers') {
+            $issuer_filter = [$this->issuer, 'ww'];
+        }
+        $filters = [
+            'code'=>[
+                'in' =>  $issuer_filter
+            ],
+            'status' => [
+                'eq' => 'published'
+            ],
+        ];
+
+        $tokenCollections = $this->directusService->getContent(
+            $collection,
+            $select,
+            $filters,
+            $options
+        );
+
+        if (empty($tokenCollections)) {
+            throw new \Exception('No collections returned for token: ' . $collection . '.' . $field);
+        }
+
+        if (count($tokenCollections) > 1) {
+            $translation = $this->getCorrectCollectionTranslation($tokenCollections);
+        } else {
+            $translation = $this->getActiveTranslation(array_first($tokenCollections)['translation']);
+        }
+
+        return array_first($translation);
+    }
+
+    /**
+     * @param array $token_details
+     * @param string $fg_id
+     * @return string
+     * @throws \Exception
+     */
+    private function getTokenTranslationFromApplication(
+        array $token_details,
+        string $fg_id
+    ): string
+    {
+        $translations = [];
+        $applicationsResponse = $this->apiService->callTlsApi('GET', '/tls/v2/'.$this->client.'/forms_in_group/' . $fg_id);
+        if($applicationsResponse['status'] != 200){
+            throw new \Exception('No applicant details returned for token: '.$token_details[1]);
+        }
+        foreach ($applicationsResponse['body'] as $applicant){
+            $translations[] = $applicant[$token_details[1]];
+        }
+        if(empty($translations)){
+            return '';
+        }
+        return implode(', ', array_unique($translations,SORT_REGULAR));
     }
 
 }

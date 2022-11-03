d8f3163dfaebb2e9528d087c75f15b41c5c0a894
Daniel Sartor <danielsartor@Daniels-MacBook-Pro.local>
2022-11-03 12:38:51 +0000

    Add conditions removing unique, fix for local and pipeline

--- database/migrations/2022_11_03_092106_update_unique_field_for_payment_accounts_table.php
+++ database/migrations/2022_11_03_092106_update_unique_field_for_payment_accounts_table.php
@@ -14,18 +14,14 @@ class UpdateUniqueFieldForPaymentAccountsTable extends Migration
     public function up()
     {
         try {
-            Schema::connection('deploy_payment_pgsql')->table('payment_accounts', function (Blueprint $table) {
-                $table->dropIndex('payment_accounts_unique_name');
-                $table->unique(['pa_name', 'pa_type'], 'payment_accounts_unique_name');
-            });
+            $this->dropUnique(['pa_name', 'pa_type']);
         } catch (\Exception $exception) {
-            $message = 'index "payment_accounts_payment_accounts_unique_name_index" does not exist';
-            if ($exception->getCode() === '42704' && str_contains($exception->getMessage(), $message)) {
+            if ($exception->getCode() === '42704') {
+                $this->dropIndex(['pa_name', 'pa_type']);
                 return;
             }
             throw $exception;
         }
-
     }
 
     /**
@@ -36,16 +32,37 @@ class UpdateUniqueFieldForPaymentAccountsTable extends Migration
     public function down()
     {
         try {
-            Schema::connection('deploy_payment_pgsql')->table('payment_accounts', function (Blueprint $table) {
-                $table->dropIndex('payment_accounts_unique_name');
-                $table->unique(['pa_name'], 'payment_accounts_unique_name');
-            });
+            $this->dropUnique(['pa_name']);
         } catch (\Exception $exception) {
-            $message = 'index "payment_accounts_payment_accounts_unique_name_index" does not exist';
-            if ($exception->getCode() === '42704' && str_contains($exception->getMessage(), $message)) {
+            if ($exception->getCode() === '42704') {
+                $this->dropIndex(['pa_name']);
                 return;
             }
             throw $exception;
         }
+    }
+
+    /**
+     * @param array $fields
+     * @return void
+     */
+    private function dropUnique(array $fields): void
+    {
+        Schema::connection('deploy_payment_pgsql')->table('payment_accounts', function (Blueprint $table) use ($fields) {
+            $table->dropUnique('payment_accounts_unique_name');
+            $table->unique($fields, 'payment_accounts_unique_name');
+        });
+    }
+
+    /**
+     * @param array $fields
+     * @return void
+     */
+    private function dropIndex(array $fields): void
+    {
+        Schema::connection('deploy_payment_pgsql')->table('payment_accounts', function (Blueprint $table) use ($fields) {
+            $table->dropIndex('payment_accounts_unique_name');
+            $table->unique($fields, 'payment_accounts_unique_name');
+        });
     }
 }

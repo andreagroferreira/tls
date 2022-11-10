f15e53eeafd2fb3238751248eecd307c564b5b43
VipinFarswan <vipinfarswan421@gmail.com>
2022-11-10 15:30:09 +0530

    update refund service

--- app/Services/RefundService.php
+++ app/Services/RefundService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\Repositories\RefundItemsRepository;
+use App\Repositories\refundItemRepository;
 use App\Repositories\RefundLogRepository;
 use App\Repositories\RefundRepository;
 use Illuminate\Support\Facades\DB;
@@ -10,22 +10,22 @@ use Illuminate\Support\Facades\DB;
 class RefundService
 {
     protected $refundRepository;
-    protected $refundItemsRepository;
+    protected $refundItemRepository;
     protected $refundLogRepository;
     protected $dbConnectionService;
 
     public function __construct(
         RefundRepository $refundRepository,
-        RefundItemsRepository $refundItemsRepository,
+        RefundItemRepository $refundItemRepository,
         RefundLogRepository $refundLogRepository,
         DbConnectionService $dbConnectionService
     ) {
         $this->refundRepository = $refundRepository;
-        $this->refundItemsRepository = $refundItemsRepository;
+        $this->refundItemRepository = $refundItemRepository;
         $this->refundLogRepository = $refundLogRepository;
         $this->dbConnectionService = $dbConnectionService;
         $this->refundRepository->setConnection($this->dbConnectionService->getConnection());
-        $this->refundItemsRepository->setConnection($this->dbConnectionService->getConnection());
+        $this->refundItemRepository->setConnection($this->dbConnectionService->getConnection());
         $this->refundLogRepository->setConnection($this->dbConnectionService->getConnection());
     }
 
@@ -36,7 +36,7 @@ class RefundService
 
         try {
             $refundRequest = $this->refundRepository->create($attributes);
-            $this->refundItemsRepository->createMany($refundRequest->r_id, $attributes['items']);
+            $this->refundItemRepository->createMany($refundRequest->r_id, $attributes['items']);
 
             $db_connection->commit();
         } catch (\Exception $e) {

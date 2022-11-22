c574dc9e5b5441a0cf34fc7eef1f2fe0f4ddba65
Preethi Jogin <preethi.jogin.ext@tlscontact.com>
2022-11-22 21:38:11 +0530

    return negative quantity for refund

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -192,11 +192,11 @@ class TransactionRepository
                 't_tech_creation',
                 'ti_id',
                 'ti_fee_type',
-                'ri_quantity AS quantity',
                 'ri_amount AS amount',
                 'ti_vat',
                 'rl_agent AS agent',
             ])
+            ->selectRaw('ri_quantity*-1 AS quantity')
             ->selectRaw('(ti_vat/100 * ri_amount)+ri_amount AS amount_gross')
             ->selectRaw('SUBSTR(t_issuer, 1, 2) AS country_code')
             ->selectRaw('SUBSTR(t_issuer, 3, 3) AS city_code');
@@ -222,11 +222,11 @@ class TransactionRepository
                 't_tech_creation',
                 'ti_id',
                 'ti_fee_type',
-                'ti_quantity AS quantity',
                 'ti_amount AS amount',
                 'ti_vat',
                 DB::raw('NULL as agent'),
             ])
+            ->selectRaw('ti_quantity AS quantity')
             ->selectRaw('(ti_vat/100 * ti_amount)+ti_amount AS amount_gross')
             ->selectRaw('SUBSTR(t_issuer, 1, 2) AS country_code')
             ->selectRaw('SUBSTR(t_issuer, 3, 3) AS city_code')

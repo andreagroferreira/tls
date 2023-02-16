3a9c45d6ca007b84eb6121922cc400d9a3e89bee
Luís Santos <luis.santos.ext@tlscontact.com>
2023-02-16 10:07:41 +0000

    fix: error while running `artisan migrate`

--- database/migrations/2023_01_23_121325_update_pc_xref_pa_id_column_to_nullable_on_payment_configurations_table.php
+++ database/migrations/2023_01_23_121325_update_pc_xref_pa_id_column_to_nullable_on_payment_configurations_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class UpdatePcXrefPaIdColumnTypeOnPaymentConfigurationsTable extends Migration
+class UpdatePcXrefPaIdColumnToNullableOnPaymentConfigurationsTable extends Migration
 {
     /**
      * Run the migrations.

204c596e7d25a127906a946480a93823888e9e55
Preethi Jogin <preethi.jogin.ext@tlscontact.com>
2023-02-24 16:03:16 +0530

    remove phpdocs for removed function parameter

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -651,7 +651,6 @@ class TransactionService
 
     /**
      * @param array $transaction
-     * @param string $paymentProvider
      *
      * @return array
      */
@@ -908,7 +907,6 @@ class TransactionService
 
     /**
      * @param array $transaction
-     * @param string $paymentProvider
      *
      * @return array
      */
